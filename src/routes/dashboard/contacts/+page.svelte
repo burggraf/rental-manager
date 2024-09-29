02641e16eb119533fccddee7bacb7457c8cbb5c7
Mark Burggraf <markb@mantisbible.com>
2024-09-29 09:03:56 -0700

    moved add user to top of page, changed icon

--- src/routes/dashboard/contacts/+page.svelte
+++ src/routes/dashboard/contacts/+page.svelte
@@ -5,7 +5,11 @@
 	import type { PageData } from './$types'
 	import { t } from '$lib/i18n'
 	import MainLayout from '../MainLayout.svelte'
-
+	import { 
+		UserPlus,
+		Menu,
+		Search,
+    } from 'lucide-svelte';
 	export let data: PageData
 
 	function handleNewContact() {
@@ -15,29 +19,31 @@
 
 <MainLayout>
 	<div slot="title">{$t('contacts.title')}</div>
-	<div slot="content">
-		<div class="flex justify-between items-center mb-4">
-			<h1 class="text-2xl font-bold">{$t('contacts.title')}</h1>
-			<Button onclick={handleNewContact} variant="outline" size="icon">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					viewBox="0 0 24 24"
-					fill="none"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					class="w-4 h-4"
-				>
-					<path d="M5 12h14" />
-					<path d="M12 5v14" />
-				</svg>
-				<span class="sr-only">New Contact</span>
-			</Button>
-		</div>
 
+	<div slot="top-right">
+		<Button onclick={handleNewContact} variant="ghost" size="icon">
+			<UserPlus class="h-5 w-5" />
+			<!-- <svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="w-4 h-4"
+			>
+				<path d="M5 12h14" />
+				<path d="M12 5v14" />
+			</svg> -->
+			<span class="sr-only">New Contact</span>
+		</Button>
+
+	</div>
+
+	<div slot="content">
 		{#if data.error}
 			<p class="error">{data.error}</p>
 		{:else if data.contacts.length === 0}
