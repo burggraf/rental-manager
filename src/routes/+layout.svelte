aa56b62e8067c90ce5ae97e81aed38d615825354
Mark Burggraf <markb@mantisbible.com>
2024-10-07 06:14:38 -0700

    added @capacitor-community/safe-area to enable var(--safe-area-inset-*) css variables

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
-  // import { onMount } from 'svelte';
-  // import { supabase } from '$lib/supabase';
-  // import { user } from '$lib/stores/authStore';
-  // import { browser } from '$app/environment';
-  import '../app.css';
-  // import { page } from '$app/stores';
-  // import LanguageSelector from '$lib/components/LanguageSelector.svelte';
-  import { ModeWatcher } from "mode-watcher";
-  // let notificationSupported = $state(false);
+	// import { onMount } from 'svelte';
+	// import { supabase } from '$lib/supabase';
+	// import { user } from '$lib/stores/authStore';
+	// import { browser } from '$app/environment';
+	import { SafeArea } from '@capacitor-community/safe-area'
+	import '../app.css'
+	// import { page } from '$app/stores';
+	// import LanguageSelector from '$lib/components/LanguageSelector.svelte';
+	import { ModeWatcher } from 'mode-watcher'
+	// let notificationSupported = $state(false);
 
-  /*
+	SafeArea.enable({
+		config: {
+			customColorsForSystemBars: true,
+			statusBarColor: '#00000000', // transparent
+			statusBarContent: 'light',
+			navigationBarColor: '#00000000', // transparent
+			navigationBarContent: 'light',
+		},
+	})
+	/*
   onMount(() => {
     if (browser) {
       notificationSupported = 'Notification' in window;
@@ -43,8 +53,8 @@
 <ModeWatcher />
 
 <svelte:head>
-  <title>{__APP_TITLE__} {__APP_VERSION__}</title>
-  <!--
+	<title>{__APP_TITLE__} {__APP_VERSION__}</title>
+	<!--
   {#if browser && notificationSupported}
     <script>
       // Request notification permission when the page loads
@@ -57,7 +67,8 @@
 </svelte:head>
 
 <!--<div class="p-safe-or-8 m-safe-or-8 overflow-auto pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)] h-[calc(100vh_-_env(safe-area-inset-top)_-_env(safe-area-inset-bottom))]">-->
-<div class="p-safe-or-8 m-safe-or-8 overflow-auto pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)] h-[calc(100vh_-_env(safe-area-inset-top)_-_env(safe-area-inset-bottom))]">
-  <slot />
+<div
+	class="p-safe-or-8 m-safe-or-8 overflow-auto pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)] h-[calc(100vh_-_env(safe-area-inset-top)_-_env(safe-area-inset-bottom))]"
+>
+	<slot />
 </div>
-
