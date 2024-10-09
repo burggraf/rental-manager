0ab23f5208253bf2b74b70c106d836a53144a8ee
Mark Burggraf <markb@mantisbible.com>
2024-10-08 20:02:58 -0700

    setup for auto-reload ios testing

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,4 +1,19 @@
 import type { CapacitorConfig } from '@capacitor/cli';
+import { networkInterfaces } from 'os';
+
+function getLocalIpAddress() {
+  const nets = networkInterfaces();
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+      if (net.family === 'IPv4' && !net.internal) {
+        return net.address;
+      }
+    }
+  }
+  return '0.0.0.0'; // Fallback
+}
+console.log('getLocalIpAddress()',getLocalIpAddress());
 
 const config: CapacitorConfig = {
   appId: 'com.dmarie.rentalmanager',
@@ -7,7 +22,11 @@ const config: CapacitorConfig = {
   bundledWebRuntime: false,
   ios: {
     contentInset: 'automatic'
-  }  
+  },
+  server: {
+    url: `http://${getLocalIpAddress()}:5173`,
+    cleartext: true
+  }
 };
 
 export default config;
